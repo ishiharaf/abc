7b579c21ac9152f858c05b0b67cf3d835f646a2a
Ishihara Felipe <k.hishihara@gmail.com>
2023-03-02 17:23:57 +0900

    Format extension.js

--- src/extension.js
+++ src/extension.js
@@ -1,12 +1,13 @@
-const vscode = require('vscode')
+const vscode = require("vscode")
+const abcJS = require("abcjs")
 
 function activate(context) {
-	console.log('Congratulations, your extension "abc" is now active!');
+	console.log('Congratulations, your extension "abc" is now active!')
 	let disposable = vscode.commands.registerCommand('abc.helloWorld', function () {
-		vscode.window.showInformationMessage('Hello World from abc!');
-	});
+		vscode.window.showInformationMessage('Hello World from abc!')
+	})
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable)
 }
 
 function deactivate() {}
