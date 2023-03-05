38b13ce38bad508689906654a5d990b39344f635
Ishihara Felipe <k.hishihara@gmail.com>
2023-03-05 21:56:34 +0900

    Refactor

--- webview/main.js
+++ webview/main.js
@@ -1,11 +1,10 @@
 const abcjs = window.ABCJS
-
 const main = document.querySelector("main")
+const button = document.querySelector("button")
 const abc = document.querySelector("abc").innerHTML
 const sheet = abcjs.renderAbc(main, abc, { responsive: "resize" })
-let synth = new abcjs.synth.CreateSynth()
+const synth = new abcjs.synth.CreateSynth()
 
-const button = document.querySelector("button")
 button.addEventListener("click", async () => {
 	const audioCtx = new AudioContext()
 
@@ -14,17 +13,19 @@ button.addEventListener("click", async () => {
 		await synth.init({
 			visualObj: sheet[0],
 			audioContext: audioCtx,
-			millisecondsPerMeasure: sheet[0].millisecondsPerMeasure()
+			millisecondsPerMeasure: sheet[0].millisecondsPerMeasure(),
+			options: {
+				onEnded: async () => {
+					await audioCtx.close()
+					button.innerText = "\uea1c"
+				}
+			}
 		})
 		await synth.prime()
 		await synth.start()
-
 		button.innerText = "\uea1e"
 
 	} else {
-		await audioCtx.close()
-		synth.stop()
-
-		button.innerText = "\uea1c"
+		await synth.stop()
 	}
 })
