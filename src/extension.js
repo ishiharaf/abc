13f34360f3741e611cd930a0ff0c7ec2b87b3c4f
Ishihara Felipe <k.hishihara@gmail.com>
2023-03-05 21:56:28 +0900

    Add MIDI output

--- src/extension.js
+++ src/extension.js
@@ -1,5 +1,7 @@
-const abcjs = require("./abcjs")
+const abcjs = require("./lib/abcjs")
 const vscode = require("vscode")
+const fs = require("fs")
+const path = require("path")
 const uri = { script: "", styles: "", abcjs: "" }
 let ctx, panel
 
@@ -17,10 +19,15 @@ module.exports = {
 }
 
 const registerCommands = () => {
-	const play = vscode.commands.registerCommand(
+	const show = vscode.commands.registerCommand(
 		"abc.showPreview", () => showPanel()
 	)
-	ctx.subscriptions.push(play)
+	ctx.subscriptions.push(show)
+
+	const create = vscode.commands.registerCommand(
+		"abc.createNew", () => createMidi()
+	)
+	ctx.subscriptions.push(create)
 }
 
 const registerEvents = () => {
@@ -35,6 +42,21 @@ const showPanel = () => {
 	initializePanel()
 	const abc = getEditorContent()
 	panel.webview.html = getWebviewContent(abc)
+}
+
+const createMidi = () => {
+	const file = getFileName(),
+		  output = getOutputPath() + ".mid"
+	const abc = getEditorContent()
+	const midi = Buffer.from(
+		abcjs.synth.getMidiFile(
+			abc, {
+				midiOutputType: "binary",
+				fileName: file + ".mid"
+			}
+		)[0]
+	)
+	writeFile(output, midi)
 }
 
 const updatePanel = () => {
@@ -59,7 +81,7 @@ const initializePanel = () => {
 		vscode.Uri.joinPath(ctx.extensionUri, 'webview', 'main.js')
 	)
 	uri.abcjs = panel.webview.asWebviewUri(
-		vscode.Uri.joinPath(ctx.extensionUri, 'src', 'abcjs.js')
+		vscode.Uri.joinPath(ctx.extensionUri, 'src/lib', 'abcjs.js')
 	)
 }
 
@@ -89,7 +111,38 @@ const getWebviewContent = (content) => {
 	`
 }
 
+const getFileName = () => {
+	const path = vscode.window.activeTextEditor.document.fileName.split("\\")
+	return path[path.length - 1].split(".")[0]
+}
+
+const getFolderName = () => {
+	const file = getFileName()
+	const path = vscode.window.activeTextEditor.document.fileName
+	return path.replace(`${file}.abc`, "")
+}
+
+const getOutputPath = () => {
+	const file = getFileName()
+	const folder = getFolderName()
+	return path.resolve(folder, file)
+}
+
 const isAbc = () => {
 	const language = vscode.window.activeTextEditor.document.languageId
 	return language == "abc" || language == "plaintext"
+}
+
+const openFile = (filename) => {
+	try {
+		return fs.readFileSync(filename).toString()
+	} catch (err) {
+		if (err) return console.error(err)
+	}
+}
+
+const writeFile = (filename, data) => {
+	fs.writeFile(filename, data, err => {
+		if (err) return console.error(err)
+	})
 }
